1e830387ebb9c742f06b2ef6cc8ee5e36dd9bfc1
David Guerin <david.guerin@developershelf.com>
2016-10-30 12:05:32 +0000

    Fixing up config.js for azure websites

--- config.js
+++ config.js
@@ -1,60 +1,49 @@
-// # Ghost Configuration
-// Setup your Ghost install for various [environments](http://support.ghost.org/config/#about-environments).
-
-// Ghost runs in `development` mode by default. Full documentation can be found at http://support.ghost.org/config/
+// # Ghost Configuration for Azure Deployment
+// Setup your Ghost install for various environments
+// Documentation can be found at http://support.ghost.org/config/
 
 var path = require('path'),
+    websiteUrl = process.env.websiteUrl,
+    websiteUrlSSL = process.env.websiteUrlSSL,
     config;
 
+// Azure Feature
+// ------------------------------------------------------------------------
+// If the App Setting 'websiteUrl' is set, Ghost will use that URL as base.
+// If it isn't set, we'll go with the default sitename.
+if (!websiteUrl || websiteUrl === '' ||  websiteUrl.length === 0) {
+    websiteUrl = 'http://' + process.env.siteName + '.azurewebsites.net';
+    console.log(websiteUrl);
+}
+
+if (!websiteUrlSSL || websiteUrlSSL === '' ||  websiteUrlSSL.length === 0) {
+    //in prod mode - forceSSL is true - so we can use the azure issued cert
+    // web apps supply some default env variables - WEBSITE_SITE_NAME and WEBSITE_HOSTNAME
+    // represent the siteName and the full DNS name respectively.
+    // using the WEBSITE_HOSTNAME we don't have to append anything and would work in ASE too.
+    websiteUrlSSL = 'https://' + process.env.WEBSITE_HOSTNAME;
+    console.log(websiteUrlSSL);
+}
+
 config = {
-    // ### Production
-    // When running Ghost in the wild, use the production environment.
-    // Configure your URL and mail settings here
-    production: {
-        url: 'http://my-ghost-blog.com',
-        mail: {},
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost.db')
-            },
-            debug: false
-        },
-
-        server: {
-            host: '127.0.0.1',
-            port: '2368'
-        }
-    },
-
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        // Change this to your Ghost blog's published URL.
-        url: 'http://localhost:2368',
+        url: websiteUrl,
 
-        // Example refferer policy
-        // Visit https://www.w3.org/TR/referrer-policy/ for instructions
-        // default 'origin-when-cross-origin',
-        // referrerPolicy: 'origin-when-cross-origin',
-
-        // Example mail config
         // Visit http://support.ghost.org/mail for instructions
-        // ```
-        //  mail: {
-        //      transport: 'SMTP',
-        //      options: {
-        //          service: 'Mailgun',
-        //          auth: {
-        //              user: '', // mailgun username
-        //              pass: ''  // mailgun password
-        //          }
-        //      }
-        //  },
-        // ```
+         mail: {
+             transport: 'SMTP',
+             options: {
+                 service: process.env.emailService,
+                 auth: {
+                     user: process.env.emailUsername, // mailgun username
+                     pass: process.env.emailPassword  // mailgun password
+                 }
+             },
+             from: process.env.emailFromAddress // 'from' address when sending emails
+         },
 
-        // #### Database
-        // Ghost supports sqlite3 (default), MySQL & PostgreSQL
         database: {
             client: 'sqlite3',
             connection: {
@@ -62,19 +51,51 @@ config = {
             },
             debug: false
         },
-        // #### Server
-        // Can be host & port (default), or socket
         server: {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            port: process.env.PORT
         },
-        // #### Paths
-        // Specify where your content directory lives
         paths: {
             contentPath: path.join(__dirname, '/content/')
-        }
+        },
+        forceAdminSSL: false
+    },
+
+    // ### Production
+    // When running Ghost in the wild, use the production environment
+    // Configure your URL and mail settings here
+    production: {
+        url: websiteUrl,
+        urlSSL: websiteUrlSSL,
+
+        // Visit http://support.ghost.org/mail for instructions
+        mail: {
+         transport: 'SMTP',
+         options: {
+             service: process.env.emailService,
+             auth: {
+                 user: process.env.emailUsername, // mailgun username
+                 pass: process.env.emailPassword  // mailgun password
+             }
+         },
+         from: process.env.emailFromAddress // 'from' address when sending emails
+        },
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost.db')
+            },
+            debug: false
+        },
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '127.0.0.1',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: process.env.PORT
+        },
+        forceAdminSSL: true
     },
 
     // **Developers only need to edit below here**
@@ -88,16 +109,7 @@ config = {
             client: 'sqlite3',
             connection: {
                 filename: path.join(__dirname, '/content/data/ghost-test.db')
-            },
-            pool: {
-                afterCreate: function (conn, done) {
-                    conn.run('PRAGMA synchronous=OFF;' +
-                    'PRAGMA journal_mode=MEMORY;' +
-                    'PRAGMA locking_mode=EXCLUSIVE;' +
-                    'BEGIN EXCLUSIVE; COMMIT;', done);
-                }
-            },
-            useNullAsDefault: true
+            }
         },
         server: {
             host: '127.0.0.1',
@@ -149,4 +161,5 @@ config = {
     }
 };
 
+// Export config
 module.exports = config;
